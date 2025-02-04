e8fdbd02fd78640a8cf790bd6f3bdf5a446b23c0
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-02-04 11:56:36 +0100

    Définition des couleurs et des polices d'écriture

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import "./App.css";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<>
-			<Navbar />
 			<Header />
 			<Outlet />
 			<Footer />
